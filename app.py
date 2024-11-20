86723696621535467e2b9fc609817814d1a08412
bibekster <megadextron911@gmail.com>
2024-11-20 16:59:48 +0545

    patient information recorded in database

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from Database.database import init_db 
+import sqlite3
 
 app = Flask(__name__)   
 
@@ -14,13 +16,24 @@ def submit():
     patientage=request.form['age']
     patientphone=request.form['phone']
 
-    return f"""
-    <h1>Submitted Information</h1>
-    <p><b>Patient-Name:</b> {patientname}</p>
-    <p><b>Patient-Email:</b> {patientemail}</p>
-    <p><b>Patient-Address:</b> {patientaddress}</p>
-    <p><b>Patient-Age:</b> {patientage}</p>
-    <p><b>Patient-Phone:</b> {patientphone}</p>
-    """
+    try:
+        conn = sqlite3.connect('patient_information.db')
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO users (name, email, address, age, phone) VALUES (?, ?, ?, ?, ?)', (patientname, patientemail, patientaddress, patientage, patientphone))
+        conn.commit()
+        conn.close()
+
+        return f"""
+        <h1>Submitted Information</h1>
+        <p><b>Patient-Name:</b> {patientname}</p>
+        <p><b>Patient-Email:</b> {patientemail}</p>
+        <p><b>Patient-Address:</b> {patientaddress}</p>
+        <p><b>Patient-Age:</b> {patientage}</p>
+        <p><b>Patient-Phone:</b> {patientphone}</p>
+        """
+    except sqlite3.IntegrityError:
+        return "<h1>Error: Email already exists!</h1>"
+
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
